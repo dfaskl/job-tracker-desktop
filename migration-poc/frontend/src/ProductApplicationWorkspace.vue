bdb9f8df17ec21f11c8304cc1e5bdb6e4cab9088
dfaskl <1830263698@qq.com>
2026-09-03 16:59:05 +0800

    feat-integrated-application-editor

---
+++ migration-poc/frontend/src/ProductApplicationWorkspace.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ProductApplications from './ProductApplications.vue'
+import MigrationApplicationCrud from './MigrationApplicationCrud.vue'
+import { useJobTrackerStore } from './jobTrackerStore'
+
+const store = useJobTrackerStore()
+</script>
+
+<template>
+  <ProductApplications />
+  <MigrationApplicationCrud v-if="store.user.value" />
+</template>
--- migration-poc/frontend/src/ProductShell.vue
+++ migration-poc/frontend/src/ProductShell.vue
@@ -2,7 +2,7 @@
 import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
 import ProductHome from './ProductHome.vue'
 import ProductStats from './ProductStats.vue'
-import ProductApplications from './ProductApplications.vue'
+import ProductApplicationWorkspace from './ProductApplicationWorkspace.vue'
 import MigrationEventCalendar from './MigrationEventCalendar.vue'
 import MigrationAiMail from './MigrationAiMail.vue'
 import MigrationSessionStatus from './MigrationSessionStatus.vue'
@@ -75,7 +75,7 @@ onBeforeUnmount(() => window.removeEventListener('hashchange', syncHash))
       <div class="page-content">
         <AccountAccess />
         <ProductHome v-if="activePage === 'home'" @navigate="navigate" />
-        <ProductApplications v-else-if="activePage === 'applications'" />
+        <ProductApplicationWorkspace v-else-if="activePage === 'applications'" />
         <MigrationEventCalendar v-else-if="activePage === 'calendar'" />
         <MigrationAiMail v-else-if="activePage === 'mail'" />
         <ProductStats v-else-if="activePage === 'stats'" />
